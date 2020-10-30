7195a9f0d243bc7adeafce929e6b8b328418d46b
ientos <73380268+ientos@users.noreply.github.com>
2020-10-29 21:32:35 -0700

    Updating Letter Trace
    Changed line orders

--- letterTrace.js
+++ letterTrace.js
@@ -1,23 +1,22 @@
 class letterTrace {
   
   show() {
+    background(143, 147, 217);
     fill('black');
     textStyle(BOLDITALIC);
     strokeWeight(0);
-    background(200, 200, 100);
     text("Letter trace", 200, 200);
     showHomeButtonLT();
   }
 }
 
 function showHomeButtonLT() {
-  //Home button
+  //home button
   textStyle(NORMAL);
+  textSize(17)
   noStroke()
   fill(255, 255, 204)
   rect(10, 10, homeButtonWidth, homeButtonHeight, 14)
   fill(100)
-  textSize(17)
-  stroke(240)
   text('Home', 40, 32);
 }
