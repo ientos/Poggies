5c1fc5c67dbe6c2fb5dda2d6156386b9fc16fa8f
ientos <73380268+ientos@users.noreply.github.com>
2020-11-29 19:13:38 -0700

    Update menuDisplay.js

--- menuDisplay.js
+++ menuDisplay.js
@@ -7,7 +7,7 @@ let dx2 = 0.8;
 
 let menuPlaySong = true;
 
-let inkLevelSettings = 400;
+let inkLevelSettings = 350;
 let livesGiven = 3;
 
 class menuDisplay {
@@ -54,18 +54,29 @@ function gameTitleNames() {
 
   //keep ups
   image(tileMap, 231, 263, 140, 80, 600, 30, 500, 380);
+  
+  //picture match
+  image(tileMap, 40, 263, 140, 80, 18, 800, 550, 380)
 
   //settings
   image(tileMap, 342, 38, 54, 20, 720, 880, 600, 150);
 
   //x close icon
   image(tileMap, 360, 9, 20, 20, 1105, 77, 150, 150);
+  
+  //?
+  image(tileMap, 360, 510, 30, 30, 2841, 60, 194, 198);
 
   //coming soon
-  // image(tileMap, 227, 373, 130, 70, 1100, 1600, 620, 280);
+  image(tileMap, 227, 373, 130, 70, 1100, 1600, 620, 280);
+  image(tileMap, 40, 373, 130, 70, 1100, 1600, 620, 280);
+  
 }
 
 function createGameOutline() {
+  if (mouseX >= 100 && mouseX <= 150 && mouseY >= 100 && mouseY <= 150) {
+    text("POG", 110, 110);
+  }
   image(tileMap, 26, 143, 180, 110, 686, 1100, 700, 450);
   image(tileMap, 211, 143, 180, 110, 686, 1100, 700, 450);
   image(tileMap, 26, 253, 180, 110, 686, 1100, 700, 450);
@@ -84,13 +95,16 @@ function createGameOutline() {
   } else if (gameState == "MENU" && mouseX >= 211 && mouseX <= 374 & mouseY >= 254 && mouseY <= 350) {
     //keep ups
     image(tileMap, 211, 253, 180, 110, 1883, 1620, 700, 450);
+  } else if (gameState == "MENU" && mouseX >= 26 && mouseX <= 189 && mouseY >= 254 && mouseY <= 350) {
+    //picture match
+    image(tileMap, 26, 253, 180, 110, 1883, 1620, 700, 450);
   } else if (gameState == "MENU" && mouseX >= 340 && mouseX <= 397 && mouseY >= 35 && mouseY <= 63) {
     //settings
     image(tileMap, 340, 35, 64, 33, 1883, 1620, 700, 450);
   } else if (gameState == "MENU" && mouseX >= 340 && mouseX <= 397 && mouseY >= 7 && mouseY <= 31) {
     //x button
     image(tileMap, 340, 6, 64, 29, 1883, 1620, 700, 450);
-  }
+  } 
 }
 
 function createMenuTitle() {
